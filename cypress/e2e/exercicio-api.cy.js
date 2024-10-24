480eb2addc1f574db1c637f76f77c6ade5e6b53e
Alexsander James <felicianoalex402@gmail.com>
2024-10-24 10:32:57 -0300

    adição de comandos customizados

--- cypress/e2e/exercicio-api.cy.js
+++ cypress/e2e/exercicio-api.cy.js
@@ -20,16 +20,9 @@ describe('Testes da Funcionalidade Usuários', () => {
   });
 
   it('Deve cadastrar um usuário com sucesso', () => {
-    cy.request ({
-      method: 'POST',
-      url: '/usuarios',
-      body: {
-        "nome": "Fulano da Silva",
-        "email": "beltrano@qa.com.br",
-        "password": "teste",
-        "administrador": "true"
-      }
-    }).should((response) =>{
+    let email = `beltrano${Math.floor(Math.random() * 100000000)}@qa.com.br`
+    cy.cadastrarUsuario('Fulano', email, 'teste', 'true')
+    .should((response) =>{
       expect(response.status).equal(201)
       expect(response.body.message).equal('Cadastro realizado com sucesso')
     })
@@ -54,30 +47,34 @@ describe('Testes da Funcionalidade Usuários', () => {
   });
 
   it('Deve editar um usuário previamente cadastrado', () => {
-    cy.request({
-      method: 'PUT',
-      url: '/usuarios/ukPDeW1GaRjCRpnJ',
-      body: {
-        "nome": "Beltrano da Silva",
-        "email": "beltrano@qa.com.br",
-        "password": "teste",
-        "administrador": "true"
-      }
-    }).should(response => {
-      expect(response.status).equal(200)
-      expect(response.body.message).equal('Registro alterado com sucesso')
-    })
+    let email = `beltrano${Math.floor(Math.random() * 100000000)}@qa.com.br`
+    cy.cadastrarUsuario('Fulano', email, '123', 'true')
+      .then(response => {
+        let id = response.body._id
+        cy.request({
+          method: 'PUT',
+          url: `/usuarios/${id}`,
+          body: {
+            "nome": "Fulano da Silva",
+            "email": email,
+            "password": "teste",
+            "administrador": "true"
+          }
+        }).should(response =>{
+          expect(response.body.message).to.equal('Registro alterado com sucesso')
+        })
+      })
+    
   });
 
   it.only('Deve deletar um usuário previamente cadastrado', () => {
-    cy.request({
-      method: 'DELETE',
-      url: '/usuarios/ukPDeW1GaRjCRpnJ'
-    }).should(response => {
-      expect(response.status).equal(200)
-      expect(response.body.message).equal('Registro excluído com sucesso | Nenhum registro excluído')
-    })
-  });
-
-
+    cy.cadastrarUsuario('Fulano', 'fulanodelete@qa.com.br', 'teste', 'true')
+      .then(response => {
+        let id = response.body._id
+        cy.request({
+          method:'DELETE',
+          url:`/usuarios/${id}`
+        })
+      })
+    });
 });
